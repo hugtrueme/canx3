f7a63e6031301f586cf5a4c04e861ebbe275b84e
Joey Chung <joey@5xruby.tw>
2018-09-28 22:30:58 +0800

    Redirect logged user to articles#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :articles
   devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  authenticated :user do
+    root to: 'articles#index', as: :authenticated_root
+  end
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
